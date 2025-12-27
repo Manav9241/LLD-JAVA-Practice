6add3a4fb9309f542299a61e8f99544ee7b9cc02
Manav9241 <manav.bansal.hsr@gmail.com>
2025-12-27 10:47:00 +0530

    ICar Interface declared

---
+++ src/OOPs/Abstraction/ICar.java
@@ -0,0 +1,9 @@
+package OOPs.Abstraction;
+
+public interface ICar {
+    public void StartEngine();
+    public void ShiftGeat(int gear);
+    public void Accelerate();
+    public void Brake();
+    public void StopEngine();
+}
