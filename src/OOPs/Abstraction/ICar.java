f29815e39e665fe4feebec118538657e7da65384
Manav9241 <manav.bansal.hsr@gmail.com>
2025-12-27 11:40:09 +0530

    Addressed Review Comments

--- src/OOPs/Abstraction/ICar.java
+++ src/OOPs/Abstraction/ICar.java
@@ -2,7 +2,7 @@ package OOPs.Abstraction;
 
 public interface ICar {
     public void StartEngine();
-    public void ShiftGeat(int gear);
+    public void ShiftGear(int gear);
     public void Accelerate();
     public void Brake();
     public void StopEngine();
