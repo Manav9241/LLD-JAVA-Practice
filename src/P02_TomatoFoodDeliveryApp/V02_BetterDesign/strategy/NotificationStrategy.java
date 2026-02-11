223bdd9db35d6b38f4b0e490d18297b3d2bbe4d7
Manav Bansal <Manav.Bansal@wisetechglobal.com>
2026-02-11 08:57:07 +0530

    Revert "Clarify Manager vs Service architectural distinction and explain Singleton pattern usage"

--- src/P02_TomatoFoodDeliveryApp/V02_BetterDesign/strategy/NotificationStrategy.java
+++
@@ -1,7 +0,0 @@
-package P02_TomatoFoodDeliveryApp.V02_BetterDesign.strategy;
-
-import P02_TomatoFoodDeliveryApp.V02_BetterDesign.model.Order;
-
-public interface NotificationStrategy {
-    void sendNotification(Order order);
-}
--- src/P02_TomatoFoodDeliveryApp/V02_BetterDesign/strategy/PaymentStrategy.java
+++
@@ -1,6 +0,0 @@
-package P02_TomatoFoodDeliveryApp.V02_BetterDesign.strategy;
-
-public interface PaymentStrategy {
-    boolean pay(double amount);
-    String getPaymentMethod();
-}
