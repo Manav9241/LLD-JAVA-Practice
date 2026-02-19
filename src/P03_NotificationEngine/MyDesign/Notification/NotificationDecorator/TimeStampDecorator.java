440c71a77f4d67ada53afe739605c73a0789ab19
Manav9241 <manav.bansal.hsr@gmail.com>
2026-02-20 03:13:34 +0530

    address: LocalDateTime.now() used to dynamically generate current timestamp

--- src/P03_NotificationEngine/MyDesign/Notification/NotificationDecorator/TimeStampDecorator.java
+++ src/P03_NotificationEngine/MyDesign/Notification/NotificationDecorator/TimeStampDecorator.java
@@ -2,12 +2,20 @@ package P03_NotificationEngine.MyDesign.Notification.NotificationDecorator;
 
 import P03_NotificationEngine.MyDesign.Notification.INotification;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class TimeStampDecorator extends INotificationDecorator{
     public TimeStampDecorator(INotification notif) {
         super(notif);
     }
 
     public String getContents() {
-        return "[2026-02-19 22:02:00]  " + this.notification.getContents();
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String formattedTimeStamp = now.format(formatter);
+        StringBuilder builder = new StringBuilder();
+        builder.append('[').append(formattedTimeStamp).append(']');
+        return builder.toString() + "  " + this.notification.getContents();
     }
 }
